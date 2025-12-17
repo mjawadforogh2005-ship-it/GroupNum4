1e8fea8644a7d3d47d7cdf24639ec48dff253a88
spongebobkzkz <130143596+spongebobkzkz@users.noreply.github.com>
2025-12-17 11:32:58 +0500

    Update Course.java

--- src/Course.java
+++ src/Course.java
@@ -1,2 +1,35 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class Course {
+    private String name;
+    private int capacity;
+    private Map<Student, Integer> students;
+
+    public Course(String name, int capacity) {
+        this.name = name;
+        this.capacity = capacity;
+        students = new HashMap<>();
+    }
+
+-    public boolean enroll(Student student) {
+        if (students.size() >= capacity) {
+            return false;
+        }
+        students.put(student, null);
+        return true;
+    }
+
+    public boolean isEnrolled(Student student) {
+        return students.containsKey(student);
+    }
+
+    public void setGrade(Student student, int grade) {
+        students.put(student, grade);
+    }
+
+    public Map<Student, Integer> getStudents() {
+        return students;
+    }
 }
+
